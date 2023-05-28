1ba349b50e0655ecedb7fd4f23114e2885d6fee2
miguel <miguelpositivo02@gmail.com>
2023-05-28 16:34:53 -0300

    exercicio resolvido

--- Program.cs
+++ Program.cs
@@ -7,34 +7,34 @@ Console.WriteLine("digite um numero");
 n1= Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("digite outro numero");
 n2 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("+ = mais  - = menos, / = divisao, % = multiplicaçao");
+Console.WriteLine("+ = mais  - = menos, / = divisao, * = multiplicaçao");
 Console.WriteLine("escolha uma das 4 operaçao a calculadora vai fazer");
 operaçao = Console.ReadLine()!;
 if (operaçao == "+" ) {
 Double resultadomais = n1 + n2;
-Console.WriteLine($"{resultadomais}");
+Console.WriteLine($"resultado = {resultadomais}");
 
 }
 if ( operaçao == "-"){
     Double resultadomenos = n1 -n2;
 
-Console.WriteLine($"{resultadomenos}");
+Console.WriteLine($"resultado = {resultadomenos}");
 
 }
-if (operaçao == "/"|| n1 != 0){
+if (operaçao == "/"){
     Double resultadodivisao = n1 / n2 ;
-Console.WriteLine($"{resultadodivisao}");
+    
+
+Console.WriteLine($" resultado = {resultadodivisao}");
 
 }
 
-else{
- Console.WriteLine("nao e possivel dividir um numero por 0");
-}
 
 
-if(operaçao =="%"){
+
+if(operaçao =="*"){
   
-  Double resultadomulti = n1 % n2;
-Console.WriteLine($"{resultadomulti}");
+  Double resultadomulti = n1 * n2;
+Console.WriteLine($"resulatdo = {resultadomulti}");
 
 }
